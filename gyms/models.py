3a6a7552ae84acfd607947d3e75ac4883aa9fea5
Sage-Park <sagepark94@gmail.com>
2023-10-07 14:31:52 +0900

    add model info

--- gyms/models.py
+++ gyms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Gym(models.Model):
+
+    """Modle Definition for Gym"""
+
+    name= models.CharField(max_length=140)
+    description = models.TextField()
+    address = models.CharField(max_length=140)
