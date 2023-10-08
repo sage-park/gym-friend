65b01bba4a15de3050241e9704ba23c4157a554c
Sage-Park <sagepark94@gmail.com>
2023-10-09 02:40:27 +0900

    custom user admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,35 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+       (
+           "Profile",
+            {
+                "fields": ("username", "password", "name", "email", "is_host")
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            }
+        ),
+        (
+            "Important dates", 
+            {
+                "fields": ("last_login", "date_joined"),
+            "classes": ("collapse",)
+            },
+
+        ),
+
+    )
+
+    list_display = ("username", "email", "name", "is_host")
